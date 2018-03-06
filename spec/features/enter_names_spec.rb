48bf6afc86873d115a7c14cde8a26fcb7c71935d
Christopher Ly <demedial@gmail.com>
2018-03-06 16:38:31 +0000

    Created web helper and method "sign_in_and_play", refactored tests

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Enter names' do
   scenario 'Players fill in names, submit, get shown on screen' do
-    visit('/')
-    fill_in 'player_1_name', with: 'FRANK'
-    fill_in 'player_2_name', with: 'FRED'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_text('Player 1: FRANK vs Player 2: FRED')
   end
 end
--- spec/features/view_hit_points_spec.rb
+++ spec/features/view_hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'View hit points' do
   scenario 'Players have hit point values after names submitted' do
-    visit('/')
-    fill_in 'player_1_name', with: 'FRANK'
-    fill_in 'player_2_name', with: 'FRED'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_text('FRED: 100SP')
   end
 end
